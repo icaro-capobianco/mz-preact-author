8aae61c5c919aac7d5e118faa2f91406d785d07a
Icaro C. Capobianco <icaro.capobianco@gmail.com>
2020-02-20 16:32:56 -0300

    MZ-2020 hooked register_author_route and added function preact_author_rest_callback

--- mz-preact-author.php
+++ mz-preact-author.php
@@ -33,16 +33,28 @@ function enqueue_style( $name, $deps = [] ) {
 function install() {
 	\add_action( 'wp_enqueue_scripts', __NAMESPACE__ . 'enqueue_assets' );
 	\add_filter( 'the_content', __NAMESPACE__ . 'insert_author_box' );
+	\add_action( 'rest_api_init', __NAMESPACE__ . 'register_author_route' );
 }
 
 function enqueue_assets() {
 	enqueue_script( 'main' );
 	enqueue_style( 'style' );
 }
-
 function insert_author_box( $content ) {
 	if ( \is_singular() ) {
 		$content .= '<div id="preact-author" ></div>';
 	}
 	return $content;
+}
+function register_author_route() {
+	$namespace = 'preact-author/v1';
+	$endpoint  = '/author/(?P<id>[\d]+)';
+	$args = [
+		'methods'  => 'GET',
+		'callback' => __NAMESPACE__ . 'preact_author_rest_callback',
+	];
+	register_rest_route( $namespace, $endpoint, $args );
+}
+function preact_author_rest_callback( $request ) {
+	$post_id = $request->getParam( 'id' );
 }
